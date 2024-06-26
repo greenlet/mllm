560e8be368cf3115ad7e405ca7b356759290aff0
Misha Burakov <misha.burakov@gmail.com>
2024-06-26 23:09:33 +0200

    Loading document-level, chunk-level metadata

--- s_03_train_mllm_level.py
+++ s_03_train_mllm_level.py
@@ -68,10 +68,12 @@ class ArgsTrain(BaseModel):
 
 def read_csv_files(ds_dir_path: Path) -> pd.DataFrame:
     dfs = []
-    for fpath in ds_dir_path.iterdir():
-        if fpath.suffix == '.csv':
-            df = pd.read_csv(fpath, header=0)
-            dfs.append(df)
+    fpaths = [p for p in ds_dir_path.iterdir() if p.suffix == '.csv']
+    n_files = len(fpaths)
+    for i in trange(n_files, desc='Processing csv files', unit='file'):
+        fpath = fpaths[i]
+        df = pd.read_csv(fpath, header=0)
+        dfs.append(df)
     df = pd.concat(dfs, axis=0)
     df.sort_values(['docid', 'offset'], inplace=True)
     df.reset_index(drop=True, inplace=True)
@@ -81,33 +83,60 @@ def read_csv_files(ds_dir_path: Path) -> pd.DataFrame:
 
 class DsLoader:
     ds_dir_path: Path
+    docs_batch_size: int
+    max_chunks_per_doc: int
     df: pd.DataFrame
-    batch_size: int
+    df_doc: pd.DataFrame
     val_ratio: float
-    n_total: int
-    n_train: int
-    n_val: int
-    chid_train: np.ndarray
-    chid_val: np.ndarray
+    n_docs: int
+    n_docs_train: int
+    n_docs_val: int
+    docids: np.ndarray
+    docids_train: np.ndarray
+    docids_val: np.ndarray
 
-    def __init__(self, ds_dir_path: Path, batch_size: int, val_ratio: float = 0.2):
+    def __init__(self, ds_dir_path: Path, docs_batch_size: int, max_chunks_per_doc: int,
+                 val_ratio: float = 0.2):
         self.ds_dir_path = ds_dir_path
+        self.docs_batch_size = docs_batch_size
+        self.max_chunks_per_doc = max_chunks_per_doc
         self.df = read_csv_files(ds_dir_path)
-        self.batch_size = batch_size
+        self.df.set_index(['docid', 'offset'], inplace=True)
+        df_doc = self.df.groupby(level=['docid'])
+        df_doc = df_doc.agg({'chid': 'count', 'title_tok_num': 'sum', 'body_tok_num': 'sum', 'tok_num': 'sum'})
+        df_doc.rename({'chid': 'chunks'}, axis=1, inplace=True)
+        self.df_doc = df_doc
         self.val_ratio = val_ratio
-        self.n_total = len(self.df)
-        self.n_val = int(self.n_total * self.val_ratio)
-        self.n_train = self.n_total - self.n_val
-        chid = self.df.chid.to_numpy()
-        np.random.shuffle(chid)
-        self.chid_train = chid[:self.n_train]
-        self.chid_val = chid[self.n_train:]
+        self.docids = self.df_doc.index.to_numpy()
+        self.n_docs = len(self.docids)
+        self.n_docs_val = int(self.n_docs * self.val_ratio)
+        self.n_docs_train = self.n_docs - self.n_docs_val
+        self.docids_train = self.docids[:self.n_docs_train]
+        self.docids_val = self.docids[self.n_docs_train:]
+        # print(self.df)
+
+    def get_batch(self, ind: int, train: bool):
+        docids = self.docids_train if train else self.docids_val
+        docids = docids[ind:ind + self.docs_batch_size]
+        dfd = self.df_doc.loc[docids]
+        for docid in docids:
+            n_chunks = self.df_doc.loc[docid]['chunks']
+            df = self.df.loc[docid]
+            print(df)
+
+
+
+
+
 
 
 def main(args: ArgsTrain) -> int:
     print(args)
 
-    ds_loader = DsLoader(args.ds_dir_path, args.batch_size)
+    docs_batch_size = 3
+    max_chunks_per_doc = 3
+    ds_loader = DsLoader(args.ds_dir_path, docs_batch_size, max_chunks_per_doc)
+    ds_loader.get_batch(1, train=True)
 
     return 0
 
