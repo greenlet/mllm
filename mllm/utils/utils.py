02242093ed36f1b775205583d5eb64cd3e850af6
Misha Burakov <misha.burakov@gmail.com>
2024-08-17 12:00:42 +0200

    Fever dataset extraction from Mteb

--- mllm/utils/utils.py
+++ mllm/utils/utils.py
@@ -1,5 +1,9 @@
+import csv
 from datetime import datetime
+from pathlib import Path
 from typing import Optional
+
+import pandas as pd
 
 
 DT_PAT = '%Y%m%d_%H%M%S'
@@ -16,4 +20,12 @@ def parse_dt_str(dt_str: str, silent: bool = True) -> Optional[datetime]:
         return datetime.strptime(dt_str, DT_PAT)
     except Exception:
         pass
+
+
+def write_tsv(df: pd.DataFrame, fpath: Path, **kwargs):
+    df.to_csv(fpath, sep='\t', header=True, quoting=csv.QUOTE_MINIMAL, index=None, **kwargs)
+
+def read_tsv(fpath: Path, **kwargs) -> pd.DataFrame:
+    df = pd.read_csv(fpath, sep='\t', header=0, quoting=csv.QUOTE_MINIMAL, **kwargs)
+    return df
 
