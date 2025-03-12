4c3f775eb53af09e5b98467047e5845f96070a1e
Mikhail Burakov <misha.burakov@gmail.com>
2025-03-12 21:57:54 +0100

    Question as an input to Encoder.

--- mllm/train/embgen_bert.py
+++ mllm/train/embgen_bert.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Optional, Generator
+from typing import Optional, Generator, Union
 
 import numpy as np
 import pandas as pd
@@ -25,12 +25,16 @@ class QnaBatch:
     q_toks: list[np.ndarray]
     a_toks: list[np.ndarray]
     qa_toks: list[np.ndarray]
+    a_att_masks: list[np.ndarray]
+    a_tgt_masks: list[np.ndarray]
     qa_att_masks: list[np.ndarray]
     qa_tgt_masks: list[np.ndarray]
     ctx_toks: np.ndarray
     qa_toks_t: list[torch.Tensor]
     q_toks_t: list[torch.Tensor]
     a_toks_t: list[torch.Tensor]
+    a_att_masks_t: list[torch.Tensor]
+    a_tgt_masks_t: list[torch.Tensor]
     qa_att_mask_t: list[torch.Tensor]
     qa_tgt_mask_t: list[torch.Tensor]
     device: Optional[torch.device] = None
@@ -50,7 +54,7 @@ class QnaBatch:
 
     def _process(self):
         # Question + Answer
-        q_toks_l, a_toks_l, qa_toks_l, qa_att_masks_l, qa_tgt_masks_l = [], [], [], [], []
+        q_toks_l, a_toks_l, qa_toks_l, a_att_masks_l, a_tgt_masks_l, qa_att_masks_l, qa_tgt_masks_l = [], [], [], [], [], [], []
         qas_sq_cum, as_cum = 0, 0
         for q, a in self.qas:
             q_toks: list[int] = self.tkz(q).input_ids
@@ -68,6 +72,17 @@ class QnaBatch:
                 continue
             qas_sq_cum += qa_len_sq
             as_cum += a_len
+
+            if self.ques_inp == QuesInp.Enc:
+                assert q_toks[-1] == self.tkz.sep_token_id
+                q_toks = q_toks[:-1]
+                n_toks = len(q_toks)
+                if n_toks > self.toks_seq_len:
+                    q_toks = q_toks[:self.toks_seq_len]
+                elif n_toks < self.toks_seq_len:
+                    n_pad = self.toks_seq_len - n_toks
+                    q_toks += [self.tkz.pad_token_id] * n_pad
+
             q_toks_l.append(np.array(q_toks, dtype=int))
             a_toks_l.append(np.array(a_toks, dtype=int))
             qa_toks_l.append(np.array(qa_toks, dtype=int))
@@ -79,11 +94,15 @@ class QnaBatch:
             a_tgt_mask = np.eye(n_a_toks, dtype=int)
             qa_att_mask = np.concatenate([q_mask, a_att_mask], axis=1)
             qa_tgt_mask = np.concatenate([q_mask * 0, a_tgt_mask], axis=1).astype(bool)
+            a_att_masks_l.append(a_att_mask)
+            a_tgt_masks_l.append(a_tgt_mask)
             qa_att_masks_l.append(qa_att_mask)
             qa_tgt_masks_l.append(qa_tgt_mask)
         self.q_toks = q_toks_l
         self.a_toks = a_toks_l
         self.qa_toks = qa_toks_l
+        self.a_att_masks = a_att_masks_l
+        self.a_tgt_masks = a_tgt_masks_l
         self.qa_att_masks = qa_att_masks_l
         self.qa_tgt_masks = qa_tgt_masks_l
 
@@ -92,10 +111,12 @@ class QnaBatch:
         max_ctx_chunks = 3
         for ctx in self.contexts:
             ctx_toks = self.tkz(ctx).input_ids
+            assert ctx_toks[0] == self.tkz.cls_token_id, f'ctx_token[0] (={ctx_toks[0]}) != cls_token_id (={self.tkz.cls_token_id})'
+            assert ctx_toks[-1] == self.tkz.sep_token_id, f'ctx_token[-1] (={ctx_toks[-1]}) != sep_token_id (={self.tkz.sep_token_id})'
+            ctx_toks = ctx_toks[:-1]
             n_pad = self.toks_seq_len - len(ctx_toks) % self.toks_seq_len
             assert self.tkz.pad_token_id is not None
             ctx_toks = np.pad(ctx_toks, (0, n_pad), constant_values=self.tkz.pad_token_id)
-            # ctx_toks = ctx_toks.reshape((-1, self.toks_seq_len))
             ctx_toks = ctx_toks[:self.toks_seq_len][None]
             ctxs.append(ctx_toks[:max_ctx_chunks])
         ctxs_all = np.concatenate(ctxs)
@@ -119,11 +140,15 @@ class QnaBatch:
     def _to_tensor_multi(self, arr: list[np.ndarray]) -> list[torch.Tensor]:
         return [self._to_tensor_single(x) for x in arr]
 
-    def gen_tensors(self) -> tuple[list[torch.Tensor], list[torch.Tensor], list[torch.Tensor], torch.Tensor]:
+    def gen_tensors(self) -> tuple[torch.Tensor, tuple[list[torch.Tensor], ...]]:
         if self.ques_inp == QuesInp.Enc:
             if self.ctx_toks_t is None:
-                pass
-            return
+                self.q_toks_t = self._to_tensor_multi(self.q_toks)
+                self.a_toks_t = self._to_tensor_multi(self.a_toks)
+                self.a_att_masks_t = self._to_tensor_multi(self.a_att_masks)
+                self.a_tgt_masks_t = self._to_tensor_multi(self.a_tgt_masks)
+                self.ctx_toks_t = self._to_tensor_single(self.ctx_toks)
+            return self.ctx_toks_t, (self.q_toks_t, self.a_toks_t, self.a_att_masks_t, self.a_tgt_masks_t)
 
         if self.ques_inp == QuesInp.Dec:
             if self.ctx_toks_t is None:
@@ -131,7 +156,7 @@ class QnaBatch:
                 self.qa_att_mask_t = self._to_tensor_multi(self.qa_att_masks)
                 self.qa_tgt_mask_t = self._to_tensor_multi(self.qa_tgt_masks)
                 self.ctx_toks_t = self._to_tensor_single(self.ctx_toks)
-            return self.qa_toks_t, self.qa_att_mask_t, self.qa_tgt_mask_t, self.ctx_toks_t
+            return self.ctx_toks_t, (self.qa_toks_t, self.qa_att_mask_t, self.qa_tgt_mask_t)
 
         raise Exception(f'Question input type {self.ques_inp} is not supported')
 
