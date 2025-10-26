4bf9fa5cb3d47143dd7469a20d49ef2fe394aefe
Misha Burakov <misha.burakov@gmail.com>
2025-10-26 22:15:59 +0100

    Distributed multi GPU Bert masked tokens predictor training example

--- s_00_01_distrib_train_example.py
+++ s_00_01_distrib_train_example.py
@@ -1,10 +1,30 @@
+import argparse
+import os
+from pathlib import Path
+
+from datasets import Dataset, load_dataset
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.nn as nn
+from torch.utils.data import DataLoader, DistributedSampler
+from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
 
-def train(rank, world_size):
+from mllm.model.bert.modeling_bert import BertModel
+
+
+class MaskedBert(nn.Module):
+    def __init__(self):
+        super(MaskedBert, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out: BaseModelOutputWithPoolingAndCrossAttentions = self.bert(x)
+        return out.last_hidden_state
+
+
+def train(dataset: Dataset, batch_size: int, rank: int = -1, world_size: int = -1):
     '''Training function for each GPU process.
 
     Args:
@@ -15,8 +35,22 @@ def train(rank, world_size):
     dist.init_process_group(backend='nccl', rank=rank, world_size=world_size)
     device = torch.device(f'cuda:{rank}')  # Set device to current GPU
 
+    # Create a DistributedSampler to ensure each GPU gets a different mini-batch
+    train_sampler = DistributedSampler(dataset)
+
+    # Create a DataLoader with the DistributedSampler
+    train_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False,  # Don't shuffle for distributed training (handled by the sampler)
+        sampler=train_sampler,
+        num_workers=4, # Sets the number of subprocesses to load data in parallel, avoiding I/O bottlenecks
+        pin_memory=True, # Copies data to pinned memory, which is faster to transfer to the GPU.
+        prefetch_factor=2 # Sets the number of batches that each worker will prepare in advance.
+    )
+
     # Instantiate the model and move it to the current GPU
-    model = MyModel().to(device)
+    model = MaskedBert().to(device)
     # Wrap the model with DDP
     ddp_model = DDP(model, device_ids=[rank])
 
@@ -35,14 +69,20 @@ def train(rank, world_size):
     # Clean up
     dist.destroy_process_group()
 
+
 if __name__ == '__main__':
-    # Get the number of GPUs
-    import argparse
+    default_data_path = Path(os.path.expandvars('$HOME/data'))
     parser = argparse.ArgumentParser()
-    parser.add_argument('--local_rank', type=int)
+    parser.add_argument('--local-rank', type=int)
+    parser.add_argument('--batch-size', type=int, default=32)
+    parser.add_argument('--data-path', type=Path, default=default_data_path)
     args = parser.parse_args()
     world_size = torch.cuda.device_count()
     local_rank = args.local_rank
+    wiki_ds_name, wiki_ds_subdir = '20220301.en', 'wikipedia'
+
+    dataset = load_dataset(wiki_ds_subdir, wiki_ds_name, cache_dir=str(args.data_path))[wiki_ds_subdir]['train']
+
     # Launch one process per GPU
-    mp.spawn(train, args=(local_rank, world_size), nprocs=world_size)
+    mp.spawn(train, args=(dataset, args.batch_size, local_rank, world_size), nprocs=world_size)
 
